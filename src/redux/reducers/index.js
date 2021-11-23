37cf2a1e0e5c50a29d46fa0b0aced67802896af1
risyad <risyadzyn@gmail.com>
2021-11-23 16:45:17 +0700

    genre modified

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,10 +1,8 @@
 import gamesReducer from './gamesReducer'
-import genresReducer from './genresReducer';
 import { combineReducers } from "redux";
 
 const rootReducer = combineReducers({
   gamesReducer,
-  // genresReducer
 });
 
 export default rootReducer;
